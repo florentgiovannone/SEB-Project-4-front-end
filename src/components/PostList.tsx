5b5c6bcd261b35a2f4668f7108bbf92802cafee1
Florent Giov annone <giovannoneflorent@gmail.com>
2024-04-19 13:21:44 +0100

    fixed heroku errro

--- src/components/PostList.tsx
+++ src/components/PostList.tsx
@@ -27,7 +27,7 @@ function postList({ user }: { user: null | IUser }) {
     }
 
     function filterPosts() {
-        return posts?.sort().filter((post: { title: string }) => {
+        return posts?.sort((a: any, b: any) => b.post_date - a.post_date).filter((post: { title: string }) => {
             const lowerSearch = search.toLowerCase();
             const lowerPostName = post.title.toLowerCase();
 
@@ -53,7 +53,7 @@ function postList({ user }: { user: null | IUser }) {
 
                 </div>}
                     <div className="columns is-multiline is-centered mb-6">
-                        {filterPosts()?.sort((a, b) => new Date(a.post_date).setHours(0, 0, 0, 0) - new Date(b.post_date).setHours(0, 0, 0, 0)).map((post) => {
+                    {filterPosts()?.map((post) => {
                         return <PostCardFull
                                 key={post.id}
                                 {...post}
