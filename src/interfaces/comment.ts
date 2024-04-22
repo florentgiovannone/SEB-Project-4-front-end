a5fd9db367c2f846699669ad966078845a4405cd
Florent Giov annone <giovannoneflorent@gmail.com>
2024-04-22 10:04:16 +0100

    fixed heroku errro

--- src/interfaces/comment.ts
+++ src/interfaces/comment.ts
@@ -1,11 +1,9 @@
 import { IUser } from "./user";
-import { IPost } from "./post";
+
 export interface IComment {
     id: string,
-    content: string
+    content: string,
     code: string
-    comment_date: string
-    post: IPost
     user: IUser
-
+    comment_date: string
 }
